03b984b485d78114992f8d4514f9c77c1dd8e740
Shabii <shabiimitjans@gmail.com>
2026-09-13 23:40:46 +0200

    adjustments

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gallery-v71-2026-09-13";
+const CACHE_VERSION = "gallery-v72-2026-09-13";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
