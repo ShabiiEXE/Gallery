273211819449651214a44920d3f3c50d65d671c8
Shabii <shabiimitjans@gmail.com>
2026-09-13 23:54:01 +0200

    Shorten slider thumb handle

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gallery-v73-2026-09-13";
+const CACHE_VERSION = "gallery-v74-2026-09-13";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
